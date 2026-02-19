dfadd4e6528f2c21e454c7a130ce44bd7f259634
Soubhagyanaik <soubhagyanaik1520@gmail.com>
2026-02-20 04:17:06 +0530

    change

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment.prod';
 
 @Injectable({ providedIn: 'root' })
 export class Api {
 
-  baseUrl = 'http://127.0.0.1:8000/api';
+  baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
